6c29296349e35a4a7387506a251eab6f78ac07b3
Vishwas Sharma <vishwasinventor@gmail.com>
2024-09-01 12:48:39 +0530

    fix: added few basic tool

--- config.nix
+++ config.nix
@@ -3,15 +3,28 @@
     nvim-environment = pkgs.buildEnv {
       name = "nvim-tools";
       paths = [
+        # System Tools
+        gcc
+        glibc
+        unzip
+        # Tools
         bash-completion
         neovim
+        tmux
+        curl
         fd
         ripgrep
         fzf
         lazygit
         awscli2
+        # Runtime
+        rustc
+        cargo
         nodejs_20
-        protobuf_26
+        # Python Runtime
+        (ruby.withPackages (ps: with ps; [ neovim ]))
+        (python312.withPackages (ps: with ps; [ pip ]))
+        pipx
       ];
     };
   };
--- install.sh
+++ install.sh
@@ -12,5 +12,28 @@ ln -sf "$PWD/.inputrc" "$HOME"/.inputrc
 ln -sf "$PWD/.tmux.conf" "$HOME"/.tmux.conf
 ln -sf "$PWD/config.nix" "$XDG_CONFIG_HOME"/nixpkgs/config.nix
 
+# Configure Nix commands
+mkdir -p ~/.config/nix
+cat <<EOF >~/.config/nix/nix.conf
+experimental-features = nix-command flakes
+EOF
+
+# Add channel
+nix-channel --add https://nixos.org/channels/nixpkgs-unstable
+nix-channel --update
+
 # install Nix packages from config.nix
 nix-env -iA nixpkgs.nvim-environment
+
+# tmux
+# mkdir ~/.config/tmux/plugins &&
+#   git clone --depth=1 https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm &&
+#   ~/.config/tmux/plugins/tpm/scripts/install_plugins.sh &&
+#   cd ~/.config/tmux/plugins/tmux-thumbs &&
+#   expect -c "spawn ./tmux-thumbs-install.sh; send \"\r2\r\"; expect complete" 1>/dev/null
+
+# nvim
+# mkdir ~/.local/nvim &&
+#   git clone --filter=blob:none --single-branch https://github.com/folke/lazy.nvim.git ~/.local/share/nvim/lazy
+# nvim --headless "+Lazy! sync" +qa
+# nvim --headless "+MasonUpdate" +qa
