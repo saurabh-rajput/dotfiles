61c2cf7c976f0c35641ab988393343304ae3145a
Vishwas Sharma <vishwasinventor@gmail.com>
2024-09-03 21:58:59 +0530

    chore: removed unwanted libraries

--- config.nix
+++ config.nix
@@ -4,7 +4,6 @@
       name = "nvim-tools";
       paths = [
         # System Tools
-        gcc
         unzip
         # Tools
         bash-completion
@@ -17,13 +16,10 @@
         lazygit
         awscli2
         # Runtime
-        rustc
-        cargo
         nodejs_20
         # Python Runtime
         (ruby.withPackages (ps: with ps; [ neovim ]))
         (python312.withPackages (ps: with ps; [ pip ]))
-        pipx
       ];
     };
   };
