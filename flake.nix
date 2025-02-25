92fba80369f4d4328d699c48fd42ef43693f19b4
Vishwas Sharma <vishwasinventor@gmail.com>
2025-02-25 20:17:26 +0530

    feat: added flake to better manage configuration

--- flake.nix
+++ flake.nix
@@ -1,36 +1,42 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils"; 
   };
 
   outputs = { self, nixpkgs, flake-utils, ... }@inputs:
-    let
-      pkgs = import nixpkgs { system = system; };
-    in pkgs.buildEnv {
-      name = "nvim-tools";
-      paths = with pkgs; [
-        # System Tools
-        unzip
-        xclip
-        coreutils
-        # Tools
-        bash-completion
-        neovim
-        tmux
-        curl
-        fd
-        ripgrep
-        fzf
-        lazygit
-        awscli2
-        direnv
-        # Runtime
-        rustc
-        cargo
-        nodejs_20
-        # Python Runtime
-        (ruby.withPackages (ps: with ps; [ neovim ]))
-      (python312.withPackages (ps: with ps; [ pip ]))
-    ];
-  };
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { system = system; };
+      in {
+        packages = {
+          default = pkgs.buildEnv {
+            name = "nvim-tools";
+            paths = with pkgs; [
+              # System Tools
+              unzip
+              xclip
+              coreutils
+              # Tools
+              bash-completion
+              neovim
+              tmux
+              curl
+              fd
+              ripgrep
+              fzf
+              lazygit
+              awscli2
+              direnv
+              # Runtime
+              rustc
+              cargo
+              nodejs_20
+              # Python Runtime
+              (ruby.withPackages (ps: with ps; [ neovim ]))
+            (python312.withPackages (ps: with ps; [ pip ]))
+          ];
+        };
+      };
+    });
 }
