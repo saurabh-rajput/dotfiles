4f85d2a2107804d9780de5ca74dcf6c361d7f9e7
Vishwas Sharma <vishwasinventor@gmail.com>
2025-02-26 18:55:04 +0530

    chore: fixing isue with config

--- nix/darwin.nix
+++ nix/darwin.nix
@@ -17,15 +17,15 @@
     enable = true; 
     ephemeral = true;
     maxJobs = 4;
-    config = {
-      virtualisation = {
-        darwin-builder = {
-          diskSize = 40 * 1024;
-          memorySize = 8 * 1024;
-        };
-      };
-      cores = 6;
-    };
+    # config = {
+    #   virtualisation = {
+    #     darwin-builder = {
+    #       diskSize = 40 * 1024;
+    #       memorySize = 8 * 1024;
+    #     };
+    #   };
+    #   cores = 6;
+    # };
   };
   nix.settings.trusted-users = [ "@admin" ];
 
