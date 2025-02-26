4321174f779807af6c67d7e4d3905519536fd17f
Vishwas Sharma <vishwasinventor@gmail.com>
2025-02-26 19:20:11 +0530

    chore: working on fixing it

--- nix/darwin.nix
+++ nix/darwin.nix
@@ -8,7 +8,7 @@
 
   # Auto upgrade nix package and the daemon service.
   # services.nix-daemon.enable = true;
-  nix.enable = false;
+  nix.enable = true;
   # nix.package = pkgs.nix;
 
   # Necessary for using flakes on this system.
