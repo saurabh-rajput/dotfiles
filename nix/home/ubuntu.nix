e81b8748e74cbd0e735d65743946e61ef0c528eb
Vishwas Sharma <vishwasinventor@gmail.com>
2025-03-11 14:50:17 +0000

    chore: fixing nothing

--- nix/home/ubuntu.nix
+++ nix/home/ubuntu.nix
@@ -1,4 +1,4 @@
-{pkgs, ...}: let
+{pkgs, lib, ...}: let
   common = pkgs.callPackage ./packages.nix {inherit pkgs;};
 in {
   # Home Manager needs a bit of information about you and the paths it should
@@ -10,5 +10,21 @@ in {
   ];
   home.packages = common.packages;
 
-  users.users.ubuntu.shell = pkgs.zsh;
+  # users.ubuntu.shell = pkgs.zsh;
+  # Set zsh as default shell on activation
+  # home.activation.make-zsh-default-shell = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+  #   # if zsh is not the current shell
+  #   PATH="/usr/bin:/bin:$PATH"
+  #   ZSH_PATH="/home/ubuntu/.nix-profile/bin/zsh"
+  #   if [[ $(getent passwd ubuntu) != *"$ZSH_PATH" ]]; then
+  #     echo "setting zsh as default shell (using chsh). password might be necessay."
+  #     if grep -q $ZSH_PATH /etc/shells; then
+  #       echo "adding zsh to /etc/shells"
+  #       run echo "$ZSH_PATH" | sudo tee -a /etc/shells
+  #     fi
+  #     echo "running chsh to make zsh the default shell"
+  #     run chsh -s $ZSH_PATH ubuntu
+  #     echo "zsh is now set as default shell !"
+  #   fi
+  # '';
 }
