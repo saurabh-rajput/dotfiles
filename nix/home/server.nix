196ca8890fb1e2244b940d0b99ea2f718ff4ef02
Vishwas Sharma <vishwasinventor@gmail.com>
2025-03-11 19:46:30 +0530

    fix: server configuration is added

---
+++ nix/home/server.nix
@@ -0,0 +1,12 @@
+{pkgs, ...}: let
+  common = pkgs.callPackage ./packages.nix {inherit pkgs;};
+in {
+  # Home Manager needs a bit of information about you and the paths it should
+  # manage.
+  home.username = "ubuntu";
+  home.homeDirectory = "/home/ubuntu";
+  imports = [
+    ./common.nix
+  ];
+  home.packages = common.packages;
+}
