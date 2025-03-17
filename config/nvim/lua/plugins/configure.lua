9a606d9c241d3b209cb2447ab104318cf8ad4073
Vishwas Sharma <vishwasinventor@gmail.com>
2025-03-17 16:38:22 +0530

    chore: added mechanism to create return type

--- config/nvim/lua/plugins/configure.lua
+++ config/nvim/lua/plugins/configure.lua
@@ -6,4 +6,15 @@ return {
       animate = { enabled = false },
     },
   },
+  {
+    "ThePrimeagen/refactoring.nvim",
+    opts = function(_, opts)
+      table.insert(opts.prompt_func_return_type, {
+        python = true,
+      })
+      table.insert(opts.prompt_func_param_type, {
+        python = true,
+      })
+    end,
+  },
 }
--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -1,23 +1,34 @@
 return {
   {
     "neovim/nvim-lspconfig",
-    opts = function(_, opts)
-      opts.inlay_hints.enabled = true
-
-      opts.servers = vim.tbl_deep_extend("force", opts.servers, {
-        clangd = {
-          mason = false,
-        },
-      })
-    end,
-
-    -- opts = {
-    --   inlay_hints = { enabled = false },
-    --   servers = {
+    -- opts = function(_, opts)
+    --   opts.inlay_hints.enabled = true
+    --
+    --   opts.servers = vim.tbl_deep_extend("force", opts.servers, {
     --     clangd = {
     --       mason = false,
     --     },
-    --   },
-    -- },
+    --   })
+    -- end,
+
+    setup = {
+      ruff = {
+        on_new_config = function(config, root_dir)
+          local env = vim.trim(vim.fn.system('cd "' .. root_dir .. '"; poetry env info -p 2>/dev/null'))
+          if string.len(env) > 0 then
+            config.settings.python.pythonPath = env .. "/bin/python"
+          end
+        end,
+      },
+    },
+
+    opts = {
+      inlay_hints = { enabled = false },
+      servers = {
+        clangd = {
+          mason = false,
+        },
+      },
+    },
   },
 }
