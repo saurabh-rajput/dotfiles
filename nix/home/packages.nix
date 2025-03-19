49e61e1a2d8ce95c92e39bb7d9908562b405ed42
Vishwas Sharma <vishwasinventor@gmail.com>
2025-03-19 23:17:50 +0530

    chore: added cdk back

--- nix/home/packages.nix
+++ nix/home/packages.nix
@@ -36,7 +36,7 @@
       rustc
       cargo
       nodejs
-      # nodePackages.aws-cdk
+      nodePackages.aws-cdk
       # Python Runtime
       (ruby.withPackages (ps: with ps; [ neovim ]))
       (python312.withPackages (ps: with ps; [ pip pyelftools ]))
