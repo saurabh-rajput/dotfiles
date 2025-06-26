a440c6eeb431263c8480731c2e46f6cd8a9a4b41
saurabh-rajput <saurabh414@gmail.com>
2025-06-26 16:50:29 -0400

    chore: trying new gemini-cli.nix

--- nix/home/gemini-cli.nix
+++ nix/home/gemini-cli.nix
@@ -5,8 +5,8 @@ pkgs.buildNpmPackage {
   version = "0.1.4"; # Or latest
 
   src = pkgs.fetchurl {
-    url = "https://registry.npmjs.org/@google/gemini-cli/-/gemini-cli-0.6.0.tgz";
-    sha256 = "sha256-DhQ8wR5APBvFHLF/+Tc+AYvPOdTpcIDqOhxsBHRwC7U"; # let it fail once to get real hash
+    url = "https://registry.npmjs.org/@google/gemini-cli/-/gemini-cli-0.1.4.tgz";
+    sha256 = "sha256-PLACEHOLDER_HASH"; # let it fail once to get real hash
   };
 
   # npmDepsHash = "sha256-PLACEHOLDER_NPM_HASH"; # let it fail once to get real hash
